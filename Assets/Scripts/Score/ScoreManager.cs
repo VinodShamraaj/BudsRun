e2e8528d1a9b2355611e6039b37ec2431de4ef19
redgamer <muhd.mustaqeem99@gmail.com>
2024-08-03 00:24:02 +0800

    remove score text

--- Assets/Scripts/Score/ScoreManager.cs
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -35,7 +35,7 @@ public class ScoreManager : MonoBehaviour
     {
         if (scoreText != null)
         {
-            scoreText.text = "Score: " + score.ToString();
+            scoreText.text = score.ToString();
         }
     }
 

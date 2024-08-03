e7b3f98f410fb9efbee687987e83a4e6a6628e48
ColonelBucket8 <freedomterran89@gmail.com>
2024-08-03 17:18:26 +0800

    FIX: Power up spawner destoy power up before duration ends

--- Assets/Scripts/Obstacles/DestroyMyself.cs
+++ Assets/Scripts/Obstacles/DestroyMyself.cs
@@ -3,11 +3,23 @@ using UnityEngine;
 
 public class DestroyMyself : MonoBehaviour
 {
+    private IPowerUpDuration powerUp;
+
+    private void Awake()
+    {
+        powerUp = GetComponent<IPowerUpDuration>();
+    }
+
     void Update()
     {
         Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
         if (screenPosition.x < -3)
         {
+            if (powerUp != null && powerUp.GetHasPickup())
+            {
+                return;
+            }
+
             Destroy(gameObject);
         }
 
